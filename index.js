963fe915dfe30bd905b2fd505f609883bf58444d
Kendra Allen <kallen5@ufl.edu>
2025-07-13 17:54:02 -0400

    modified index.js

--- index.js
+++ index.js
@@ -15,12 +15,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const apiKey = "9bffa1724249c6bb0a35f38003f5f95b";
     var weatherURL = "https://api.openweathermap.org/data/2.5/weather";
-    var queryString = "?units=imperial&appid=yourapikeyhere&q=" + userQuery;
+    var queryString = "?units=imperial&appid=" + apiKey + "&q=" + userQuery;
     var fetchURL = weatherURL + queryString;
 
 
     try {
-      const response = await fetch(fetchUrl);
+      const response = await fetch(fetchURL);
       if (!response.ok) throw new Error("Location not found");
 
       const data = await response.json();
@@ -42,8 +42,18 @@ document.addEventListener("DOMContentLoaded", () => {
       var mapLink = `https://www.google.com/maps/search/?api=1&query=${lat},${lon}`;
       var iconUrl = `https://openweathermap.org/img/wn/${condition.icon}@2x.png`;
 
+        weatherSection.innerHTML = `
+        <h2 id="example-loc">${city}, ${country}</h2>
+        <a id="example-link" href="${mapLink}" target="__BLANK">Click to view map</a>
+        <img id="example-icon" src="${iconUrl}" alt="${condition.description}">
+        <p id="example-desc" style="text-transform: capitalize;">${condition.description}</p><br>
+        <p id="example-actual-temp">Current: ${temp.toFixed(2)}° F</p>
+        <p id="example-feels-temp">Feels like: ${feels_like.toFixed(2)}° F</p><br>
+        <p id="example-time">Last updated: ${timeString}</p>
+        `;
+
     } catch (error) {
-      weatherSection.innerHTML = `<h2>Location not found</h2>`;
+      weatherSection.innerHTML = `Location not found`;
     }
   });
 });
